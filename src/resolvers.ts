0cc46c9123b09783084e447fdd636973019af154
kothru <1894683+kothru@users.noreply.github.com>
2022-04-02 22:06:56 +0900

    WIP mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+// import { PrismaClient, Task } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
@@ -6,4 +7,14 @@ export const resolvers = {
   Query: {
     tasks: async () => await prisma.task.findMany()
   },
+  // Mutation: {
+  //   updateActual: async (_: any, args: Task[]) => {
+  //     args.forEach(async (task) => {
+  //       await prisma.task.update({
+  //         where: { id: task.id },
+  //         data: { actual: task.actual }
+  //       })
+  //     })
+  //   }
+  // }
 }
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,4 +1,5 @@
 import { makeSchema, objectType, queryType, list } from 'nexus'
+// import { makeSchema, objectType, queryType, list, mutationType, nonNull, intArg, stringArg } from 'nexus'
 
 const Task = objectType({
   name: 'Task',
@@ -17,6 +18,23 @@ const Query = queryType({
     })
   },
 })
+
+// const Mutation = mutationType({
+//   definition(t) {
+//     t.nonNull.field('updateActual', {
+//       type: list(Task),
+//       args: list(Task),
+//       resolve(_root, args, ctx) {
+//         let draftToPublish = ctx.db.tasks.find(p => p.id === args.draftId)
+//         if (!draftToPublish) {
+//           throw new Error('Could not find draft with id ' + args.draftId)
+//         }
+//         draftToPublish.published = true
+//         return draftToPublish
+//       },
+//     })
+//   },
+// })
 
 export const typeDefs = makeSchema({
   types: [Task, Query],
